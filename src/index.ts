18ee8d591308654b4eb1d959673ff2f021a03958
Thiago Felix <tfelix@laterpay.net>
2023-10-04 07:56:00 -0300

    feat: simple auth with demo page (#4)
    * feat: simple auth with demo page
    
    * fix: adding unit tests to auth module

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,50 @@
-import { Configuration, InternalApi } from "@laterpay/tapper-sdk";
+import {
+  Configuration,
+  InternalApi,
+  UserIdentityApi,
+} from "@laterpay/tapper-sdk";
 
-export function auth() {
-  console.log("Doing authentication");
+import {
+  handleAuthWindow,
+  authorize,
+  authenticate,
+  refreshAuthentication,
+  AuthOptions,
+  Authentication,
+} from "./auth";
+
+const getAuthentication = () => {
+  const entry = localStorage.getItem("supertab-auth");
+  return entry ? (JSON.parse(entry) as Authentication) : null;
+};
+
+const setAuthentication = (auth: Authentication) => {
+  localStorage.setItem("supertab-auth", JSON.stringify(auth));
+  return auth;
+};
+
+export async function auth(options: AuthOptions & { silently: boolean }) {
+  const previousAuth = getAuthentication();
+  const isExpired = (previousAuth?.expiresAt || 0) < Date.now();
+
+  if (previousAuth && !isExpired) {
+    return previousAuth;
+  } else if (previousAuth && isExpired) {
+    const authentication = await refreshAuthentication({
+      ...options,
+      ...previousAuth,
+    });
+    return setAuthentication(authentication);
+  } else if (!options.silently) {
+    const { url, codeVerifier } = await authorize(options);
+    const authCode = await handleAuthWindow(url);
+    const authentication = await authenticate({
+      ...options,
+      codeVerifier,
+      authCode,
+    });
+    return setAuthentication(authentication);
+  }
 }
 
 export async function getApiVersion() {
@@ -12,4 +55,13 @@ export async function getApiVersion() {
   const healthCheck = await new InternalApi(config).health();
 
   return healthCheck.version;
+}
+
+export async function getCurrentUser({ accessToken }: { accessToken: string }) {
+  const config = new Configuration({
+    basePath: "https://tapi.sbx.laterpay.net",
+    accessToken: `Bearer ${accessToken}`,
+  });
+
+  return new UserIdentityApi(config).getCurrentUserV1();
 }
