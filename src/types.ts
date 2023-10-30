b4b591c36b68abd69d0d00c0de4726bf3bd27a63
Tom Stark <tomasstark@pm.me>
2023-10-30 14:48:16 +0100

    Fix merge error

--- src/types.ts
+++ src/types.ts
@@ -3,11 +3,3 @@ import { AuthStatus } from "./auth";
 export interface Authenticable {
   authStatus: AuthStatus;
 }
-<<<<<<< HEAD
-
-export enum AccessStatus {
-  GRANTED = "Granted",
-  DENIED = "Denied",
-}
-=======
->>>>>>> feat/CL-1102/access-check
