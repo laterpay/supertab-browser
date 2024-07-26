82bc37b0b1afa0045ad82f98be666aa8a4776765
Sławek Ehlert <sehlert@laterpay.net>
2024-07-26 17:22:55 +0200

    Define AUTH_URL and TOKEN_URL everywhere

--- tsup.stg.config.ts
+++ tsup.stg.config.ts
@@ -11,6 +11,8 @@ export default defineConfig({
   minify: false,
   env: {
     AUTH_BASE_URL: "https://auth.stg.laterpay.net",
+    AUTH_URL: "https://signon.stg.supertab.co/oauth2/auth",
+    TOKEN_URL: "https://auth.stg.supetab.co/oauth2/token",
     SSO_BASE_URL: "https://signon.stg.supertab.co",
     TAPI_BASE_URL: "https://tapi.stg.laterpay.net",
     CHECKOUT_BASE_URL: "https://checkout.stg.supertab.co",
