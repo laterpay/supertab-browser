bded9806ef06e8c9be27b76b1527f61ab4883329
Tom Stark <tomasstark@pm.me>
2024-08-08 13:55:40 +0200

    add beforeSend to filter out errors not relevent to SDK bundle

--- src/sentry.ts
+++ src/sentry.ts
@@ -15,16 +15,16 @@ Sentry.init({
       ? "stg"
       : "prod",
   integrations: [Sentry.browserTracingIntegration()],
-  //  beforeSend: (event: Sentry.ErrorEvent) => {
-  //    const found = event.exception?.values?.some(
-  //      (value) =>
-  //        value.stacktrace?.frames?.some(
-  //          (frame) => frame.filename?.match(/tab\.js|bundle\.js/)
-  //        )
-  //    );
+  beforeSend: (event: Sentry.ErrorEvent) => {
+    const found = event.exception?.values?.some(
+      (value) =>
+        value.stacktrace?.frames?.some(
+          (frame) => frame.filename?.match(/dist\/(prod|sbx|stg)\/index\.js/),
+        ),
+    );
 
-  //    return found ? event : null;
-  //  },
+    return found ? event : null;
+  },
   tracesSampleRate: 1.0, //  Capture 100% of the transactions
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
 });
